679166efa9a071c0dbe9e6c870fa62fbb6b20971
Gary Chan <gary.chan@qualicom.com>
2024-06-13 16:51:46 -0400

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "gary.chan@qualicom.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "gary.chan@qualicom.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
