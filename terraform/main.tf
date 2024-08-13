85d7718d039923ba1f84a0fd9e9a1f8471862ee8
Gary Chan <gary.chan@qualicom.com>
2024-08-13 11:38:31 -0400

    test account request 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,33 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+
+
+module "sandbox2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sandbox-aft2@qualicom.com"
+    AccountName               = "sandbox-aft2"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "gary.chan@qualicom.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT2"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT2"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform 2"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox2"
+}
