fc44aa7ab7a66dcf7136aa70636abb39c4aefe8d
Gary Chan <gary.chan@qualicom.com>
2024-08-14 12:55:30 -0400

    test sandbox 3 without vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,33 @@ module "sandbox2" {
 
   account_customizations_name = "sandbox2"
 }
+
+
+
+module "sandbox3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "sandbox-aft3@qualicom.com"
+    AccountName               = "sandbox-aft3"
+    ManagedOrganizationalUnit = "Learn AFT"
+    SSOUserEmail              = "gary.chan@qualicom.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT3"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT3"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform 3"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
